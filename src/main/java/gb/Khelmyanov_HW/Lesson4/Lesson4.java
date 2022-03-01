418e08864db562c2de3d7920dbaa832a00ec1879
Khelmyanov_Alexander <alkhelmyanov@gmail.com>
2022-03-01 22:19:55 +0300

    Метод main создан.

--- src/main/java/gb/Khelmyanov_HW/Lesson4/Lesson4.java
+++ src/main/java/gb/Khelmyanov_HW/Lesson4/Lesson4.java
@@ -1,4 +1,7 @@
 package gb.Khelmyanov_HW.Lesson4;
 
 public class Lesson4 {
+    public static void main(String[] args) {
+        //Создать вначале игры меню с Настройками, где будет выбор играть с человеком или компьютером, плюс размер поля, плюс кол-во знаков в ряд для победы.
+    }
 }
