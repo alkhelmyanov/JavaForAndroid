8b893aefff3f55b0b150984205dc988a90f5a926
Khelmyanov_Alexander <alkhelmyanov@gmail.com>
2022-04-09 15:49:34 +0300

    ДЗ №10. Выполненное 1е задание. Чтобы подсчитать количество повторяющихся слов был взят метод frequency из класса Collections.

--- src/main/java/gb/Khelmyanov_HW/Lesson10/DictionaryOfWords.java
+++ src/main/java/gb/Khelmyanov_HW/Lesson10/DictionaryOfWords.java
@@ -1,8 +1,6 @@
 package gb.Khelmyanov_HW.Lesson10;
 
-
 import java.util.*;
-
 
 public class DictionaryOfWords {
 
@@ -11,13 +9,13 @@ public class DictionaryOfWords {
         // Создаем коллекцию с различными словами
         List<String> dictionaryOfWords = new ArrayList<>();
 
-        dictionaryOfWords.add("вум"); // долина по Саамски
-        dictionaryOfWords.add("вуон"); // залив по Саамски
-        dictionaryOfWords.add("вуон"); // залив по Саамски
-        dictionaryOfWords.add("йок"); // река по Саамски
-        dictionaryOfWords.add("корч"); // сквозное ущелье в горах по Саамски
-        dictionaryOfWords.add("корч"); // сквозное ущелье в горах по Саамски
-        dictionaryOfWords.add("лаг"); // пологий участок склона горы по Саамски
+        dictionaryOfWords.add("вум");     // долина по Саамски
+        dictionaryOfWords.add("вуон");    // залив по Саамски
+        dictionaryOfWords.add("вуон");    // залив по Саамски
+        dictionaryOfWords.add("йок");     // река по Саамски
+        dictionaryOfWords.add("корч");    // сквозное ущелье в горах по Саамски
+        dictionaryOfWords.add("корч");    // сквозное ущелье в горах по Саамски
+        dictionaryOfWords.add("лаг");     // пологий участок склона горы по Саамски
         dictionaryOfWords.add("ламбина"); // проточное озеро по Саамски
         dictionaryOfWords.add("озеро");
         dictionaryOfWords.add("лес");
@@ -27,20 +25,20 @@ public class DictionaryOfWords {
         dictionaryOfWords.add("море");
         dictionaryOfWords.add("Сопка");
         dictionaryOfWords.add("Сопка");
-        dictionaryOfWords.add("нярк"); //  мыс, полуостров по Саамски
-        dictionaryOfWords.add("суол"); //  остров по Саамски
-        dictionaryOfWords.add("чорр"); //   горный массив с плоской вершиной по Саамски
-        dictionaryOfWords.add("чорр"); //   горный массив с плоской вершиной по Саамски
-        dictionaryOfWords.add("чорр"); //   горный массив с плоской вершиной по Саамски
-        dictionaryOfWords.add("явр"); //   озеро по Саамски
-        dictionaryOfWords.add("озеро"); //   озеро по Саамски
-        dictionaryOfWords.add("озеро"); //   озеро по Саамски
-        dictionaryOfWords.add("луг"); //   озеро по Саамски
+        dictionaryOfWords.add("нярк");   //  мыс, полуостров по Саамски
+        dictionaryOfWords.add("суол");   //  остров по Саамски
+        dictionaryOfWords.add("чорр");   //   горный массив с плоской вершиной по Саамски
+        dictionaryOfWords.add("чорр");   //   горный массив с плоской вершиной по Саамски
+        dictionaryOfWords.add("чорр");   //   горный массив с плоской вершиной по Саамски
+        dictionaryOfWords.add("явр");    //   озеро по Саамски
+        dictionaryOfWords.add("озеро");  //   озеро по Саамски
+        dictionaryOfWords.add("озеро");  //   озеро по Саамски
+        dictionaryOfWords.add("луг");    //   озеро по Саамски
         dictionaryOfWords.add("хребет"); //   озеро по Саамски
 
         System.out.println("Имеется коллекция dictionaryOfWords :\n" + dictionaryOfWords);
 
-        // Создаем сортированный список с исключительными словами из колекциии dictionaryOfWords
+        // Создаем сортированный список с исключительными словами из колекциии dictionaryOfWords,  тут же их сравниваем и считаем
 
         Set<String> dictionaryOfWordsUnic = new HashSet<>(dictionaryOfWords);
         System.out.println("\nВ коллекции dictionaryOfWords следующие слова повторяются:");
