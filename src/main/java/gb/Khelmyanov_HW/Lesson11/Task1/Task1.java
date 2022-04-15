adf5d7a59af0251bd30b96dc91358f097b0b0512
Khelmyanov_Alexander <alkhelmyanov@gmail.com>
2022-04-15 18:44:35 +0300

    ДЗ11. Задание #1, мудрил, оказалось есть метод swap

--- src/main/java/gb/Khelmyanov_HW/Lesson11/Task1/Task1.java
+++ src/main/java/gb/Khelmyanov_HW/Lesson11/Task1/Task1.java
@@ -2,6 +2,7 @@ package gb.Khelmyanov_HW.Lesson11.Task1;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 public class Task1 {
     public static void main(String[] args) {
@@ -12,12 +13,17 @@ public class Task1 {
         ArrayList<String> arrayListStr = new ArrayList<>();
         Collections.addAll(arrayListStr, "Саша", "Маша", "Галя", "Валя", "Глаша");
 
+        ArrayList<Object> arrayListObj  = new ArrayList<>();
+        Collections.addAll(arrayListObj, arrayListInt,arrayListInt,arrayListStr,arrayListStr );
+
         System.out.println(arrayListInt);
         System.out.println(arrayListStr);
+        System.out.println(arrayListObj);
 
         ArrrayTask arrrayTask = new ArrrayTask();
         arrrayTask.changeArrayElementsPosition(arrayListInt, 2, 3);
         arrrayTask.changeArrayElementsPosition(arrayListStr,2,3);
+        arrrayTask.changeArrayElementsPosition(arrayListObj,1,2);
     }
 
 
@@ -25,10 +31,15 @@ public class Task1 {
 
 class ArrrayTask {
 
+    // СНачала описывал сложно сам, а потом оказалось что есть стандартный метод через swap
     public static <T> void changeArrayElementsPosition(ArrayList<T> arrayList, Integer Element1, Integer Element2) {
 
-        Collections.swap(arrayList,Element1, Element2);
-        System.out.println(arrayList);
+        final ArrayList <T> ORIGINALARRAYLIST = arrayList;
+        System.out.println("Оригинальная коллекция: " + ORIGINALARRAYLIST);
+
+        arrayList.set(Element1, arrayList.set(Element2, arrayList.get(Element1)));
+        System.out.println("Измененная коллекция: " + arrayList);
 
     }
+
 }
